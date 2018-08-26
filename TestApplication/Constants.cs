0c89b9fccca61adb313f79e40351c653083c473f
ramgab58 <ramgab58@yahoo.com.ph>
2018-08-26 13:16:10 -0400

    Initial check-in of module CmsVendorApp

---
+++ TestApplication/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace TestApplication
+{
+    public static class Constants
+    {
+        public const string UsernameHC = "webtester";
+        public const string PasswordHC = "CustMover12!";
+    }
+}
---
+++ TestApplication/LoginPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TestApplication
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Awesome Title", "Displaying awesome alert!", "Cancel");
+        }
+
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            var user = new User{
+                Username = UserIdText.Text,
+                Password = PasswordText.Text
+            };
+
+            if(CheckCredentials(user))
+            {
+                Navigation.InsertPageBefore(new MainPage(), this);
+                await Navigation.PopAsync();
+            }
+            else {
+                await DisplayAlert("Error", 
+                             "The User ID or Password you entered is incorrect. Please click 'OK' to reenter your User ID and Password",
+                            "OK");
+            }
+        }
+
+        bool CheckCredentials(User obj)
+        {
+            if(obj.Username.Equals(Constants.UsernameHC) && obj.Password.Equals(Constants.PasswordHC)){
+                return true;
+            }
+            return false;
+        }
+    }
+}
